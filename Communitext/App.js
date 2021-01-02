a90818d14cede29f00f3d647a01aec9675ee7cab
Victoria Pavlov <victoria.pavlov75@myhunter.cuny.edu>
2021-01-02 18:12:01 -0500

    added subcategory page

--- Communitext/App.js
+++ Communitext/App.js
@@ -2,11 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View} from 'react-native';
 import CategoryPage from './components/CategoryPage';
-
+import SubCategoryPage from './components/SubCategoryPage';
 const App = () => {
   return(
     <View>
       <CategoryPage />
+      <SubCategoryPage />
     </View>
     
   )
