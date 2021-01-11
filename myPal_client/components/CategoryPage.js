f8988876081f4392c98f8c6cd52206758e4b9398
Andrew Alagna <aalagna04@gmail.com>
2021-01-11 13:32:03 -0500

    Combine models to one file (models.js)
    Still need to finish seeding

---
+++ myPal_client/components/CategoryPage.js
@@ -0,0 +1,51 @@
+import React,{useState} from 'react';
+import {
+    View,
+    Text,
+    TouchableOpacity,
+    Image,
+    ScrollView,
+    TextInput,
+    Button
+} from 'react-native';
+
+
+const CategoryPage = ({navigation}) => {
+    const [categories, setCategories] = useState(["category1", "category2", "category3", "category4", "category5", "category6"]);
+    const [Category, setCategory] = useState("");
+
+    const addCategory = (newCategory) => {
+        if(newCategory != "")
+        {
+            setCategories(oldState => [...oldState, newCategory]);
+        }
+    }
+
+    return(
+        <ScrollView>
+            {categories.map((category,index) => (
+         <TouchableOpacity
+                        key = {index}
+                        onPress={() =>
+                            navigation.navigate('Subcategory', {category: category})}>
+                        <Image source = {require('../assets/Images/Categories/Animals/1.png')}/>
+                        <Text>
+                            {category}
+                        </Text>
+         </TouchableOpacity>
+
+
+                   ))}
+            <TextInput
+                value = {Category}
+                placeholder = "Enter new category"
+                onChangeText = {text =>
+                    setCategory(text)} />
+            <Button title = "Submit"
+                    onPress = {() => {addCategory(Category);
+                    setCategory("")}}/>
+        </ScrollView>
+    )
+        }
+
+export default CategoryPage;
