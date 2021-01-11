e3c842396dbfcf5fafbe21aaa24d523d02a5b361
Andrew Alagna <aalagna04@gmail.com>
2021-01-11 12:01:49 -0500

    update models

---
+++ Communitext/backend/DB_mod/index.js
@@ -0,0 +1,53 @@
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+
+/**
+ * Create new Sequelize instance
+ * @const sequelize
+ */
+const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: 'myPal.db',
+    define: {
+        timestamps: true,
+    },
+    logging: false,
+});
+
+console.log('Testing the connection to the database...');
+(async () => {
+    try {
+        // Test the connection to the database
+        console.log('Connection to the database successful!_!');
+        await sequelize.authenticate();
+
+    } catch(error) {
+        console.log(error);
+    }
+})();
+
+const models = {};
+
+// Import all of the models.
+fs
+    .readdirSync(path.join(__dirname, 'models'))
+    .forEach((file) => {
+        console.info(`Importing database model from file: ${file}`);
+        const model = sequelize.import(path.join(__dirname, 'models', file));
+        models[model.name] = model;
+    });
+
+// If available, call method to create associations.
+Object.keys(models).forEach((modelName) => {
+    if (models[modelName].associate) {
+        console.info(`Configuring the associations for the ${modelName} model...`);
+        models[modelName].associate(models);
+    }
+});
+
+module.exports = {
+    sequelize,
+    Sequelize,
+    models,
+};
---
+++ Communitext/backend/DB_mod/models.js
@@ -0,0 +1,148 @@
+ 
+const Sequelize = require('sequelize')
+const db_sequel = new Sequelize({
+    dialect: 'sqlite',
+    storage: 'myPal.db',
+    define: {
+        timestamps: true,
+    },
+    logging: false,
+})
+
+const Category = db_sequel.define('category', {
+  CategoryID: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  catName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  images: {
+    type: Sequelize.TEXT,
+  },
+})
+
+const SubCategory = db_sequel.define('subcategory', {
+  SubCategoryID: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+
+  subName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+
+  images: {
+    type: Sequelize.TEXT,
+  },
+
+})
+
+const Sentence = db_sequel.define('sentences', {
+  SentenceID: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+
+  sentence: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+})
+
+
+Category.hasMany(SubCategory, {foreignKey: 'campusId'})
+SubCategory.belongsTo(Category)
+SubCategory.hasMany(Sentence)
+Sentence.belongsTo(SubCategory)
+
+const sync = async () => {
+  await db_sequel.sync({force: true})
+  const [hunter, johnjay, nyu] = await Promise.all([
+    Category.create({
+      name: 'Hunter College',
+      imageUrl:
+        'http://www.hunter.cuny.edu/admissions/repository/images/classic-hunter-nyc-cabs.jpg',
+      address: 'idk street',
+      description: 'descr1',
+    }),
+    Category.create({
+      name: 'John Jay',
+      imageUrl:
+        'https://www.collegeconsensus.com/wp-content/uploads/2016/12/CUNY-John-Jay-College-Criminal-Justice.jpg',
+      address: 'idk street1',
+      description: 'descr2',
+    }),
+    Category.create({
+      name: 'NYU',
+      imageUrl:
+        'https://steinhardt.nyu.edu/sites/default/files/styles/facebook/public/2019-08/nyc_school_buildings_9G0-WrtQ.jpg?h=6413f173&itok=w7Y5nRcZ',
+      address: 'idk street2',
+      description: 'descr3',
+    }),
+  ])
+  await Promise.all([
+    SubCategory.create({
+      firstName: 'stanley',
+      lastName: 'lim',
+      email: 'asdas@gmail.com',
+      GPA: 4.0,
+      campusId: hunter.id,
+    }),
+    SubCategory.create({
+      firstName: 'stanley1',
+      lastName: 'lim1',
+      email: 'asdas1@gmail.com',
+      GPA: 0.0,
+      campusId: hunter.id,
+    }),
+    SubCategory.create({
+      firstName: 'guywithfirstname',
+      lastName: 'guywithlastname',
+      email: 'guywithemail@gmail.com',
+      GPA: 3.0,
+      campusId: nyu.id,
+    }),
+    SubCategory.create({
+      firstName: 'toyinwithfirstname',
+      lastName: 'toyinwithlastname',
+      email: 'toyinwithemail@gmail.com',
+      GPA: 4.0,
+      campusId: johnjay.id,
+    }),
+  ])
+}
+
+module.exports = {
+  sync,
+  models: {
+    Campus: Category,
+    Student: SubCategory,
+  },
+}
