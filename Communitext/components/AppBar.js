fea08fc70d333c6ecf35ca7cad28d3dc48b5d9f8
David Neuman <davidsneuman@gmail.com>
2021-01-17 17:50:57 -0500

    center nav text

--- Communitext/components/AppBar.js
+++ Communitext/components/AppBar.js
@@ -103,7 +103,7 @@ const styles = StyleSheet.create({
     fontSize: 18,
     alignItems: 'center',
     // textDecorationLine: 'underline',
-    //   textAlign:"center"
+    textAlign:"center",
   },
 });
 
